8938ea1f353327d6b78ff4c64fc2514bd7042cab
Stéfano López <stefano.lopez.mdp@gmail.com>
2025-10-23 10:48:04 -0300

    Agregamos Landing Page, nuevo logo y botones para iniciar sesión

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { Landing } from './features/landing/landing';
 
 export const routes: Routes = [
+  { path: '', component: Landing },  // <-- Página inicial
   {
     path: 'book',
     loadComponent: () => import('./features/book/form/book').then(m => m.BookComponent)
   },
-  { path: '', redirectTo: 'book', pathMatch: 'full' },
-  { path: '**', redirectTo: 'book' }
+  { path: '**', redirectTo: '' } // Redirige cualquier ruta desconocida al landing page
 ];
